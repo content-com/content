7c6c0001dd4ea2616d4c8ec35ef8698513ebc687
Lucasbelich <lucasbelich1@gmail.com>
2022-10-26 00:41:18 -0300

    register

---
+++ pages/register.js
@@ -0,0 +1,13 @@
+const register = () => {
+    return (
+      <div className="register">
+        <div className="register__header">
+          <h2>Create account</h2>
+          <h4>Connect with Google or Sign up with your Email.</h4>
+        </div>
+        <div><button></button></div>
+      </div>
+    );
+  };
+  
+  export default register;
