60b6e0b640637a2e9a2668f9970158de2f2184b8
EzeRomanelli <ezeromanelli@hotmail.com>
2022-11-24 12:30:41 -0300

    Adding conditional navbar with new icons added

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -1,23 +1,45 @@
 import Link from "next/link";
+import Image from 'next/image'
+import styles from "../../styles/Home/NavBar/NavBar.module.css";
 
-const Navbar = () => {
+const Navbar = ({ loggedIn = true }) => {
+
+  console.log(loggedIn)
   return (
     <nav className="navbar">
       <Link href="/">
         <a>Content.</a>
       </Link>
-      <div>
-        {/* <Link href="/profile">
-          <a>
+        {
+          loggedIn ? (<div className={styles.navBarIconsDivLoggedIn}>
             <Image
-              width="20"
-              height="20"
-              alt="Profile navbar icon"
-              src="/profile.svg"
-            ></Image>
-          </a>
-        </Link> */}
-      </div>
+              width={24}
+              height={24}
+              src="/NotificationsIcon.svg"
+              alt='notifications icon'
+            />
+            <Image
+              width={24}
+              height={24}
+              src="/ChatsIcon.svg"
+              alt='chats icon'
+            />
+            <Image
+              width={24}
+              height={24}
+              src="/ProfileIcon.svg"
+              alt='profile icon'
+            />
+          </div>):
+          <div className={styles.navBarIconsDiv}>
+          <Image
+          width={24}
+          height={24}
+          src="/ProfileIcon.svg"
+          alt='notifications icon'
+          />
+        </div>
+        }
     </nav>
   );
 };
